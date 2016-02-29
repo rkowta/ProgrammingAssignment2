4fbf5d83e97761db92cbee2cf226598f760e8932
rkowta <rkowta@gmail.com>
2016-02-28 21:15:45 -0600

    Revert "Programmin Assignment 2"
    This reverts commit 5972268c8c08dbfe09d86da2b8b05b35cb2a52dd.

--- cachematrix.R
+++ cachematrix.R
@@ -1,43 +1,15 @@
-## makeCacheMatrix caches both the input matrix, and its inverse, so that the 
-## inverse of a matrix is calculated only once.
-## cacheSolve returns the inverse of matrix.
+## Put comments here that give an overall description of what your
+## functions do
 
-## makeCacheMatrix accpets a matrix as input and returns a special "matrix" object
-## which stores both the matrix and its inverse. It does this by using a closure
-## function.
+## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-    inv <- NULL
-    setMatrix <- function(y) {
-        x <<- y
-        inv <<- NULL
-    }
-    getMatrix <- function(y) x
-    setInverse <- function(y) inv <<- y
-    getInverse <- function(y) inv
-    list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
+
 }
 
 
-## cacheSolve function accepts a matrix as input and returns the inverse of the matrix.
-## If the input to cacheSolve function is the special matrix created by makeCacheMatrix
-## function, then the inverse is calculated only one time on the first call.
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-    if ("getInverse" %in% names(x)){
-        inv <- x$getInverse()
-    }
-    else{
-        return(solve(x))
-    }
- 
-    if (!is.null(inv)){
-        message("getting cached data for inverse")
-        return(inv)
-    }
-    mat1 <- x$getMatrix()
-    inv <- solve(mat1)
-    x$setInvers(inv)
-    inv
 }
